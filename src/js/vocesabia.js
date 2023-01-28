1f518aa634a370475f8056580c4b2dbe7b53da8c
CahPazza <carinepazza@gmail.com>
2023-01-28 17:19:06 -0300

    adicionado o js para a selecao

---
+++ src/js/vocesabia.js
@@ -0,0 +1,59 @@
+
+const lista = [
+    {
+        selecao: "Brasil",
+        bandeira: "",
+        ANOS: [1958, 1962, 1970, 1994, 2002],
+    },
+    {
+        selecao: "Alemanha",
+        bandeira: "",
+        ANOS: [1954, 1974, 1990, 2014], 
+    },
+    {
+        selecao: "Itália",
+        bandeira: "",
+        ANOS: [1934, 1938, 1982, 2006], 
+    },
+    {
+        selecao: "Argentina",
+        bandeira: "",
+        ANOS: [1978, 1986, 2022], 
+    },
+    {
+        selecao: "Uruguai",
+        bandeira: "",
+        ANOS: [1930, 1950], 
+    },
+    {
+        selecao: "França",
+        bandeira: "",
+        ANOS: [1998, 2018], 
+    },
+    {
+        selecao: "Espanha",
+        bandeira: "",
+        ANOS: [2010], 
+    },
+    {
+        selecao: "Inglaterra",
+        bandeira: "",
+        ANOS: [1966], 
+    },
+
+    <img src="src/img/icon/trofeu.png" alt="Troféu"></img>
+    
+
+]
+
+const trofeus = document.querySelector(".trofeu")
+trofeu.innerHTML=""
+
+for (let i=0; i < selecoes [0].ANOS.length; i++)
+{trofeu.innerHTML +- '<img src="src/img/icon/trofeu.png" alt="Troféu">'}
+
+for ( i in lista ){
+    <span>list [i].selecao</span>
+}
+
+
